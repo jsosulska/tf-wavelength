1965936052eb7ed8191c3a76bb767b43a6c9a4cb
Kerim Satirli <k.satirli@gmail.com>
2021-03-05 17:57:25 +0100

    linting

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,10 @@
 terraform {
+  terraform_version = "0.14.7"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "3.31.0"
     }
   }
-}
-
-provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
 }
